ef0c5cdacc02c1837a409cbba1364676d1988f3d
herpestes <ahmetbeyazoglu@gmail.com>
2023-06-25 18:55:32 +0300

    SignedIn

--- app/src/main/java/com/herpestes/cloneinstagram/IgViewModel.kt
+++ app/src/main/java/com/herpestes/cloneinstagram/IgViewModel.kt
@@ -10,6 +10,7 @@ import com.herpestes.cloneinstagram.data.UserData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+const val USERS = "users"
 
 @HiltViewModel
 class IgViewModel @Inject constructor(
@@ -22,6 +23,31 @@ class IgViewModel @Inject constructor(
     val inProgress = mutableStateOf(false)
     val userData = mutableStateOf<UserData?>(null)
     fun onSignup(username: String, email: String, pass: String){
+        inProgress.value = true
+
+        db.collection(USERS).whereEqualTo("username", username).get()
+            .addOnSuccessListener { documents ->
+                if(documents.size() > 0 ){
+                    handleException(customMessage = "Username already exists")
+                    inProgress.value = false
+                }else{
+                    auth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener {  task ->
+                            if(task.isSuccessful){
+                                    signedIn.value = true
+                                //create profile
+                                }else{
+                                    handleException(task.exception, "Signup Failed")
+                            }
+                            inProgress.value = false
+                        }
+                }
+
+            }
+            .addOnFailureListener {  }
+    }
+
+    fun handleException(exception: Exception? = null, customMessage: String? = ""){
 
     }
 
