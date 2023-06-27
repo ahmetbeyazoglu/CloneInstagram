37b863ddbe231260efd39d5a53dbfc9ace6283e6
herpestes <ahmetbeyazoglu@gmail.com>
2023-06-27 17:47:52 +0300

    MyPost, Search and icon

---
+++ app/src/main/java/com/herpestes/cloneinstagram/main/FeedScreen.kt
@@ -0,0 +1,25 @@
+package com.herpestes.cloneinstagram.main
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.herpestes.cloneinstagram.IgViewModel
+
+@Composable
+fun FeedScreen(navController: NavController, vm: IgViewModel) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Feed Screen")
+        }
+        
+        BottomNavigationMenu(
+            selectedItem = BottomNavigationItem.FEED,
+            navController = navController
+        )
+
+    }
+
+}
